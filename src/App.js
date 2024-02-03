7d45fdba2a746c7d55f3968c0bf81517cf045cd4
heesus2 <likeheesu@gmail.com>
2024-02-04 03:36:41 +0900

    react 공부

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './component/Hello';
+import State from './component/State'
 
 function App() {
   return (
-    <>
-      <div>
-        
-      </div>
-    </>
+    <div>
+      <State />
+      <State />
+      <State />
+    </div>
   );
 }
 
